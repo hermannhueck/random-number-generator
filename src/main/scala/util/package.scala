048a32b3b01991523747adaa3158978e68646b93
Hermann Hueck <hermann.hueck@mailbox.org>
2019-12-30 22:09:05 +0100

    added util

---
+++ src/main/scala/util/package.scala
@@ -0,0 +1,38 @@
+package object util {
+
+  def printStartLine(width: Int = 80, color: String = Console.GREEN): Unit =
+    println(lineStart(width, color))
+
+  def printEndLine(width: Int = 80): Unit =
+    println(lineEnd(width))
+
+  def printLine(width: Int = 80): Unit =
+    println(line(width))
+
+  def line(width: Int = 80): String =
+    "\u2500" * width
+
+  def lineStart(width: Int = 80, color: String = Console.GREEN): String =
+    line(width) ++ color
+
+  def lineEnd(width: Int = 80): String =
+    Console.RESET ++ line(width)
+
+  implicit class StringSyntax(private val what: String) extends AnyVal {
+
+    def boxed(width: Int = 80): String =
+      s"${line(width)}\n$what\n${line(width)}"
+
+    def colored(escape: String): String =
+      s"$escape$what${Console.RESET}"
+
+    def red: String =
+      what.colored(Console.RED)
+
+    def green: String =
+      what.colored(Console.GREEN)
+
+    def blue: String =
+      what.colored(Console.BLUE)
+  }
+}
