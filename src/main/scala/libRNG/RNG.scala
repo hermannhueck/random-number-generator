7f6868ea0ac6f34d1f37810d671f514c3f665c04
Hermann Hueck <hermann.hueck@mailbox.org>
2019-12-31 22:16:18 +0100

    refactored RNG

--- src/main/scala/libRNG/RNG.scala
+++ src/main/scala/libRNG/RNG.scala
@@ -5,9 +5,12 @@ case class RNG(seed: Long) {
   // Linear congruential generator:
   // https://en.wikipedia.org/wiki/Linear_congruential_generator
   //
+  private def generate(seed: Long): Long =
+    (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
+  // `&` is bitwise AND. We use the current seed to generate a new seed.
+
   private def nextRNG: RNG =
-    // `&` is bitwise AND. We use the current seed to generate a new seed.
-    RNG((seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL)
+    RNG(generate(seed))
 
   def nextLong: (RNG, Long) = {
     val rng: RNG = nextRNG
